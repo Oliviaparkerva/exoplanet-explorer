df2c9ddf952be1b6998be4bc9aaf6ffbc3df15ee
Olivia Parker <oliviaparkerva@gmail.com>
2018-10-08 12:54:47 -0400

    Student Answer
    Used Fetch API, Personalized comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,8 +3,7 @@ Instructions:
 (1) Rewrite get with the Fetch API: https://davidwalsh.name/fetch
 (2) Finish the getJSON method. getJSON should take a URL and return the parsed JSON response.
   (a) getJSON needs to return a Promise!
-(3) Test by console.logging the response and by passing the query string from getJSON to addSearchHeader.
-(4) Handle errors by passing "unknown" to addSearchHeader.
+
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
@@ -20,7 +19,7 @@ Instructions:
    * @param {String} query - The search query.
    */
   function addSearchHeader(query) {
-    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+    home.innerHTML = '<h2 class="page-title">query: ' + query+ '</h2>';
   }
 
   /**
@@ -29,12 +28,9 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    Use the Fetch API to GET a URL.
-    Return the fetch.
-
-    Your code goes here!
-     */
+    return fetch(url,{
+      method: 'get'
+    });
   }
 
   /**
@@ -43,21 +39,29 @@ Instructions:
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
-    /*
-    Return a Promise that gets a URL and parses the JSON response. Use your get method!
-
-    Your code goes here!
-     */
+    /*Return a Promise that gets a URL and parses the JSON response. Use your get method from above .then return the response in json format*/
+    return get(url).then((response)=>{
+      return response.json();
+    });
   }
+  
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
     /*
-    Uncomment the next line when you're ready to test!
-    Don't forget to chain with a .then and a .catch!
-
-    Your code goes here too!
+    (3) Test: 
+      .then the getJSON(url) response and pass the response query string from getJSON to addSearchHeader(query) function 
+      .catch any error ; console.log the error 
+    (4) Handle errors by passing "unknown" to addSearchHeader.
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then((response)=>{
+        addSearchHeader(response.query)
+        console.log(response);
+      })
+      .catch((error)=>{
+        addSearchHeader('unknown');
+        console.log(error);
+      })
   });
 })(document);
